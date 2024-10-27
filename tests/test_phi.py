72cf43fbedc9508e34a5be37d7dbba803995c27a
William Gurecky <gureckywl@ornl.gov>
2024-10-27 19:41:00 -0400

    reorder tests

--- tests/test_phi.py
+++ tests/test_phi.py
@@ -24,70 +24,6 @@ ref_phi = np.array([
     [ 0.10808308959542341,  0.1266237199582164,   0.15124928069313592,
       0.18476090064484876,  0.23154914510216867,  0.29863201236633136]])
 
-def test_phipm_unstable():
-    """
-    Test that the phipm correctly computes linear combinations
-    of phi-vector products:
-
-    phipm
-    ==
-    phi_1*(dt*A)*(dt)*b1 +
-    phi_2*(dt*A)*(dt)*b2
-
-    where
-    phipm = phi_0(dt*A)*0 + phi_1*(dt*A)*(dt)*b1 + phi_2*(dt*A)*(dt)*b2
-    """
-    # make a random A matrix
-    n = 10
-    np.random.seed(42)
-    np_test_a = np.random.rand(n,n) * np.eye(10) * -1.
-    test_a = jnp.asarray(np_test_a, dtype=jnp.float64)
-    test_a_lo = JaxMatrixLinop(test_a)
-
-    # arbitrary vectors
-    test_b0 = jnp.asarray(np.zeros(n))
-    test_b1 = jnp.asarray(np.linspace(0.2, 3.4, n)) * 1e-2
-    test_b2 = jnp.asarray(np.linspace(0.8, 4.0, n)) * 1e-1
-    # some random stepsize
-    dt = 0.1
-
-    base_phi_1_b1 = f_phi_k_ext(dt*test_a, 1) @ test_b1
-    base_phi_2_b2 = f_phi_k_ext(dt*test_a, 2) @ test_b2
-    base_phi_combo = base_phi_1_b1 + base_phi_2_b2
-
-    # compute same thing using phipm method
-    vb = [test_b0, test_b1, test_b2]
-    p = 2
-    phipm_phi_combo = phipm_unstable(test_a_lo, dt, vb, p, max_krylov_dim=n+1, iom=10, n_steps=2)
-    # assert jnp.allclose(phipm_phi_combo, base_phi_combo, atol=1e-3)
-
-    # === small bateman test case for phipm
-    n = 3
-    np_test_a = np.array([
-        [-1e-3, 1.e-1, 0.0],
-        [   0.,-1.e-1, 1e1],
-        [   0.,    0.,-1e1],
-        ])
-    test_a = jnp.asarray(np_test_a, dtype=jnp.float64)
-    test_a_lo = JaxMatrixLinop(test_a)
-
-    # arbitrary small vectors with vastly different magnitudes
-    test_b0 = jnp.asarray(np.zeros(n))
-    test_b1 = jnp.asarray(np.linspace(0.2, 3.4, n) * 1e-1)
-    test_b2 = jnp.asarray(np.linspace(0.8, 4.0, n) * 1e-4)
-    dt = 5.0
-
-    base_phi_1_b1 = f_phi_k_ext(dt*test_a, 1) @ test_b1
-    base_phi_2_b2 = f_phi_k_ext(dt*test_a, 2) @ test_b2
-    base_phi_combo = base_phi_1_b1 + base_phi_2_b2
-
-    # compute same thing using phipm method
-    vb = [test_b0, test_b1, test_b2]
-    p = 2
-    phipm_phi_combo = phipm_unstable(test_a_lo, dt, vb, p, max_krylov_dim=n, iom=3, n_steps=2)
-    assert jnp.allclose(phipm_phi_combo, base_phi_combo, atol=1e-3)
-
-
 def test_phi_0():
     """
     Computes phi_0(A) == exp(A)
@@ -205,16 +141,10 @@ def test_phi_linop_1():
 
 def test_kiops_fixedstep():
     """
-    Test that the phipm correctly computes linear combinations
-    of phi-vector products:
+    Test that the KIOPS method correctly computes
+    linear combinations of phi-vector products:
 
-    kiops
-    ==
-    phi_1*(dt*A)*(dt)*b1 +
-    phi_2*(dt*A)*(dt)*b2
-
-    where
-    kiops = phi_0(dt*A)*0 + phi_1*(dt*A)*(dt)*b1 + phi_2*(dt*A)*(dt)*b2
+    kiops_w = phi_0(dt*A)*0 + phi_1*(dt*A)*(dt)*b1 + phi_2*(dt*A)*(dt)*b2
     """
     # === small bateman test case for phipm
     n = 3
@@ -241,3 +171,48 @@ def test_kiops_fixedstep():
     p = 2
     phipm_phi_combo = kiops_fixedsteps(test_a_lo, dt, vb, p, max_krylov_dim=10, iom=10, n_steps=1)
     assert jnp.allclose(phipm_phi_combo, base_phi_combo, atol=1e-6)
+
+
+def test_phipm_unstable():
+    """
+    NOTE: This test currently fails
+    TODO: phipm with substepping impl is broken.
+
+    Test that the phipm correctly computes linear combinations
+    of phi-vector products:
+
+    phipm
+    ==
+    phi_1*(dt*A)*(dt)*b1 +
+    phi_2*(dt*A)*(dt)*b2
+
+    where
+    phipm = phi_0(dt*A)*0 + phi_1*(dt*A)*(dt)*b1 + phi_2*(dt*A)*(dt)*b2
+    """
+    # === small bateman test case for phipm
+    n = 3
+    np_test_a = np.array([
+        [-1e-3, 1.e-1, 0.0],
+        [   0.,-1.e-1, 1e1],
+        [   0.,    0.,-1e1],
+        ])
+    test_a = jnp.asarray(np_test_a, dtype=jnp.float64)
+    test_a_lo = JaxMatrixLinop(test_a)
+
+    # arbitrary small vectors with vastly different magnitudes
+    test_b0 = jnp.asarray(np.zeros(n))
+    test_b1 = jnp.asarray(np.linspace(0.2, 3.4, n) * 1e-1)
+    test_b2 = jnp.asarray(np.linspace(0.8, 4.0, n) * 1e-4)
+    dt = 2.5
+
+    base_phi_1_b1 = f_phi_k_ext(dt*test_a, 1) @ test_b1
+    base_phi_2_b2 = f_phi_k_ext(dt*test_a, 2) @ test_b2
+    base_phi_combo = base_phi_1_b1 + base_phi_2_b2
+
+    # compute same thing using phipm method
+    vb = [test_b0, test_b1, test_b2]
+    p = 2
+    phipm_phi_combo = phipm_unstable(test_a_lo, dt, vb, p, max_krylov_dim=n, iom=3, n_steps=2)
+    assert jnp.allclose(phipm_phi_combo, base_phi_combo, atol=1e-6)
+
+
