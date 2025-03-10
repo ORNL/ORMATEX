fix bug in linearization of rhs accounting for time derivative of the frhs in nonautonomous systems

examples/ex_ormatex_rspy.py
@@ -14,6 +14,10 @@ from ormatex_py.ode_sys import OdeSys, OdeSysNp, MatrixLinOp
 jax.config.update("jax_enable_x64", True)
 
 
+@jax.jit
+def f_pred_hunt(t):
+    return 0.4*(jnp.sin(t*0.2)+1.0)
+
 # Define the nonautonomous ODE system
 class LotkaVolterra(OdeSys):
     alpha: float
@@ -31,7 +35,7 @@ class LotkaVolterra(OdeSys):
     @jax.jit
     def _frhs(self, t, x, **kwargs):
         # hunter populations are known functions of time
-        pred_hunt = 0.4*(jnp.sin(t*0.2)+1.0)
+        pred_hunt = f_pred_hunt(t)
         # pred prey time derivatives
         prey_t = self.alpha * x[0] - self.beta * x[0] * x[1]
         pred_t = self.delta * x[0] * x[1] - self.gamma * x[1] - pred_hunt*x[1]
@@ -51,7 +55,9 @@ def run_model(method="exprb2_rs", tol_fdt=1.0e-6):
     else:
         y0 = jnp.array(y0.flatten())
         res = integrate(LotkaVolterra(), y0, t0, dt, nsteps, method=method, tol_fdt=tol_fdt)
-    return res.t_res, res.y_res
+    y0 = jnp.array(y0.flatten())
+    res_expected = integrate(LotkaVolterra(), y0, t0, dt, nsteps, method="dopri5")
+    return res.t, res.y, res_expected.t, res_expected.y
 
 if __name__ == "__main__":
     import matplotlib.pylab as plt
@@ -60,15 +66,18 @@ if __name__ == "__main__":
     parser.add_argument("-method", help="Integration method", type=str, default="exprb2_rs")
     parser.add_argument("-tol_fdt", help="Nonautonomous system check threshold", type=float, default=1.0e-6)
     args = parser.parse_args()
-    t_out, y_out = run_model(args.method, args.tol_fdt)
+    t_out, y_out, t_true, y_true = run_model(args.method, args.tol_fdt)
     # Visualize results
     print(y_out)
     plt.figure()
     plt.plot(t_out, y_out[:, 0], label='prey')
     plt.plot(t_out, y_out[:, 1], label='pred')
-    plt.plot(t_out, 0.4*(np.sin(t_out*0.2)+1.0), label='predator hunters')
+    plt.plot(t_true, y_true[:, 0], ls='--', label='prey true')
+    plt.plot(t_true, y_true[:, 1], ls='--', label='pred true')
+    mae = np.mean(np.abs(y_out[:, 0] - y_true[:, 0]))
+    plt.plot(t_out, f_pred_hunt(t_out), label='predator hunters')
     plt.grid(ls='--')
-    plt.title("Method: %s" % (args.method))
+    plt.title("Method: %s, MAE err: %0.3e" % (args.method, mae))
     plt.legend()
-    plt.savefig("ormatex_rspy_lv.png")
+    plt.savefig("ormatex_rspy_lv_%s.png" % (str(args.method)))
     plt.close()

ormatex_py/ode_exp.py
@@ -45,14 +45,18 @@ class ExpRBIntegrator(IntegrateSys):
         super().reset_ic(t0, y0)
 
     def _remf(self, tr: float, yr: jax.Array,
-              frhs_yt: jax.Array, sys_jac_lop_yt: LinOp):
+              frhs_yt: jax.Array, sys_jac_lop_yt: LinOp, v=0.0):
         """
-        Computes remainder R(yr) = frhs(yr) - frhs(yt) - J_yt*(yr-yt)
+        Computes remainder R(yr) = frhs(yr) - frhs(yt) - J_yt*(yr-yt) - v*(tr-t0)
+        where
+        v = d(frhs)/dt
         """
+        t = self.t_hist[0]
         yt = self.y_hist[0]
+        dt = tr - t
         frhs_yr = self.sys.frhs(tr, yr)
         jac_yd = sys_jac_lop_yt(yr - yt)
-        return frhs_yr - frhs_yt - jac_yd
+        return frhs_yr - frhs_yt - jac_yd - v*dt
 
     def _phi2v_nonauto(self, sys_jac_lop, dt):
         r"""
@@ -74,8 +78,8 @@ class ExpRBIntegrator(IntegrateSys):
             fytt = jax.jacfwd(self.sys.frhs, argnums=0)(t, yt)
             # check for nonautonomous system
             if jnp.linalg.norm(fytt, ord=jax.numpy.inf) > self.tol_fdt:
-                return (dt**2.)*phi_linop(sys_jac_lop, dt, fytt, 2, self.max_krylov_dim, self.iom)
-        return 0.
+                return (dt**2.)*phi_linop(sys_jac_lop, dt, fytt, 2, self.max_krylov_dim, self.iom), fytt
+        return 0., 0.
 
     def _step_exprb2(self, dt: float) -> StepResult:
         """
@@ -89,9 +93,10 @@ class ExpRBIntegrator(IntegrateSys):
         sys_jac_lop = self.sys.fjac(t, yt)
         fyt = self.sys.frhs(t, yt)
         fyt_dt = fyt * dt
+        phi2_v, _0 = self._phi2v_nonauto(sys_jac_lop, dt)
         y_new = yt \
             + phi_linop(sys_jac_lop, dt, fyt_dt, 1, self.max_krylov_dim, self.iom) \
-            + self._phi2v_nonauto(sys_jac_lop, dt)
+            + phi2_v
         # no error est. avail
         y_err = -1.
         return StepResult(t+dt, dt, y_new, y_err)
@@ -141,12 +146,15 @@ class ExpRBIntegrator(IntegrateSys):
         sys_jac_lop = self.sys.fjac(t, yt)
         fyt = self.sys.frhs(t, yt)
 
+        # phi2_v is nonzero for nonautonomous systems
+        phi2_v, v = self._phi2v_nonauto(sys_jac_lop, dt)
+
         # 2nd stage
         t_2 = t + dt
         y_2 = yt \
             + dt*phi_linop(sys_jac_lop, dt, fyt, 1, self.max_krylov_dim, self.iom) \
-            + self._phi2v_nonauto(sys_jac_lop, dt)
-        r_2 = self._remf(t_2, y_2, fyt, sys_jac_lop)
+            + phi2_v
+        r_2 = self._remf(t_2, y_2, fyt, sys_jac_lop, v=v)
 
         # compute final update
         y_new = y_2 + 2.*dt*phi_linop(sys_jac_lop, dt, r_2, 3,
@@ -276,14 +284,18 @@ class ExpSplitIntegrator(IntegrateSys):
         self._cached_dt = None
 
     def _remf(self, tr: float, yr: jax.Array,
-              frhs_yt: jax.Array, sys_lop: LinOp):
+              frhs_yt: jax.Array, sys_lop: LinOp, v=0.0):
         """
-        Computes remainder R(yr) = frhs(yr) - frhs(yt) - L*(yr-y0)
+        Computes remainder R(yr) = frhs(yr) - frhs(yt) - L*(yr-y0) - v*(tr-t0)
+        where
+        v = d(frhs)/dt
         """
+        t = self.t_hist[0]
         yt = self.y_hist[0]
+        dt = tr - t
         frhs_yr = self.sys.frhs(tr, yr)
         L_yd = sys_lop(yr - yt)
-        return frhs_yr - frhs_yt - L_yd
+        return frhs_yr - frhs_yt - L_yd - v*dt
 
     def _step_exp1(self, dt: float) -> StepResult:
         """