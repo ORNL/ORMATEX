fdd14ee9b7101eacb777880cc9885a9a8b03cc9f
William Gurecky <gureckywl@ornl.gov>
2025-03-10 17:18:10 -0400

    adds python regression test for nonautonomous problem

--- tests/test_regression_lotka_volterra.py
+++ tests/test_regression_lotka_volterra.py
@@ -5,7 +5,7 @@ Check that all exponential integrators solve a simple
 nonlinear system to the expected precission.
 """
 from ormatex_py import integrate_wrapper
-from ormatex_py.progression.lotka_volterra import LotkaVolterra
+from ormatex_py.progression.lotka_volterra import LotkaVolterra, LotkaVolterraNonauto
 import numpy as np
 import scipy as sp
 import jax
@@ -37,6 +37,50 @@ def test_lotka_volterra():
             y0 = jnp.array([0.1, 0.2])
             res = integrate_wrapper.integrate(
                     sys, y0, t0, dt, nsteps, method, max_krylov_dim=10, iom=5)
+            t_res, y_res = res.t, res.y
+            t_res = np.asarray(t_res)
+            y_res = np.asarray(y_res)
+            diff = y_res - y_true
+            mae = np.mean(np.abs(diff))
+            print("Method: %s, dt: %0.4e, mean abs err: %0.4e" % (method, dt, mae))
+            assert mae < 1e-2
+            err_dt.append([dt, mae])
+        err_dt = np.array(err_dt)
+
+        # estimate conv order
+        trendf = lambda x, s, b: s*np.log(x)+b
+        popt, _ = sp.optimize.curve_fit(trendf, err_dt[:, 0], np.log(err_dt[:, 1]), p0=[1.0, 1.0])
+        est_order = popt[0]
+        print("Method: %s, Est conv order: %0.4e" % (method, est_order))
+        order_tol = 1e-1
+        assert est_order - order > (0.0 - order_tol)
+
+
+def test_lotka_volterra_nonautonomous():
+    """
+    Test exponential integrators on the forced, nonautonomous Lotka-Volterra system.
+    This system contains an additional term that is only a function of time.
+    """
+    sys = LotkaVolterraNonauto()
+    dt_list = [0.01, 0.0125, 0.02, 0.05]
+    methods = ["exprb2", "exprb3"]
+    methods_order = [2.0, 3.0]
+    for method, order in zip(methods, methods_order):
+        err_dt = []
+        for dt in dt_list:
+            y0 = jnp.array([0.1, 0.2])
+            # compute expected result
+            t0 = 0.0
+            tf = 50.0
+            nsteps = int(tf/dt)
+            res_true = integrate_wrapper.integrate(
+                    sys, y0, t0, dt, nsteps, "dopri5")
+            t_true, y_true = res_true.t_res, res_true.y_res
+
+            # compute ormatex result
+            y0 = jnp.array([0.1, 0.2])
+            res = integrate_wrapper.integrate(
+                    sys, y0, t0, dt, nsteps, method, max_krylov_dim=10, iom=5, tol_fdt=1.0e-6)
             t_res, y_res = res.t, res.y
             t_res = np.asarray(t_res)
             y_res = np.asarray(y_res)
